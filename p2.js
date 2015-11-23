d2b67711baf18f5cd074ca4ace68da84a8008267
mraop1994 <a.philp1994@gmail.com>
2015-11-23 08:45:18 +0000

    Second commit3

---
+++ p2.js
@@ -0,0 +1,40 @@
+function ValidationEvent() {
+    var name1 = document.getElementById("firstname").value;
+    var name2 = document.getElementById("lastname").value;
+    var cnsty = document.getElementById("cnsty").value;
+    var email = document.getElementById("email").value;
+    var years = document.getElementById("years").value;
+    var psw1 = document.getElementById("psw1").value;
+    var psw2 = document.getElementById("psw2").value;
+    var emailReg = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if (name1 != '' && name2 != '' && cnsty != '' && email != '' && years != '' && psw1 != '' && psw2 != '') {
+        if (email.match(emailReg)) {
+            if (years >= 0 && years <= 50) {
+                if (psw1 == psw2) {
+                    alert("Thanks for your submission.");
+                    return true;
+                } else {
+                    alert("Passwords don't match!!");
+                    document.getElementById("psw1").style.backgroundColor = "red";
+                    document.getElementById("psw1").value = "";
+                    document.getElementById("psw2").style.backgroundColor = "red";
+                    document.getElementById("psw2").value = "";
+                    return false;
+                }
+            } else {
+                alert("Invalid number!!");
+                document.getElementById("years").style.backgroundColor = "red";
+                document.getElementById("years").value = "";
+                return false;
+            }
+        } else {
+            alert("Invalid Email Address...!!!");
+            document.getElementById("email").style.backgroundColor = "red";
+            document.getElementById("email").value = "";
+            return false;
+        }
+    } else {
+        alert("All fields are required.....!");
+        return false;
+    }
+}
